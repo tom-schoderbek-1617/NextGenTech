581a9ea45c48136b9c037b998860390beb037500
Carson-Key-1516 <Moldyvermin@keywebserver.site.nfoservers.com>
2017-05-26 13:14:44 -0700

    Add files via upload

---
+++ agar.js
@@ -0,0 +1,337 @@
+const playerSpeed = 150;
+const magnetRange = 300;
+const magnetStrength = 15;
+const blobCount = 4000; // Count of pellets to pick up (These are locally rendered and created)
+const worldSize = 12000; // World size
+const blobSize = 20; // Count of pellets to pick up (These are locally rendered and created)
+const startingSize = 1000; // Starting size of the player
+const worldBorder = worldSize + blobSize / 2; // World border
+const zoomScale = startingSize * 0.6;
+
+let player;
+let zoom = 10;
+let mycolor;
+let prepareFirebase = false;
+let players;
+let hitBorder = false;
+let listening = true;
+let lifetimePoints = 0;
+let lastScore = startingSize;
+
+let prevX = 0;
+let prevY = 0;
+let prevR = 0;
+
+let blobs = [];
+let colorMap = {};
+let recentlyEaten = {};
+let highScores = {};
+
+let averageFPS = 0;
+let countFPS = 0;
+
+
+let glitchCounter = 0;
+let speedCounter = 0;
+let zoomCounter = 1;
+let magnetCounter = 0;
+
+function setup() {
+    createCanvas(window.innerWidth,  window.innerHeight - document.getElementById('topbar').offsetHeight);
+    colorMode(HSB, 100);
+    mycolor = random(100);
+    player = new Blob(random(-worldSize, worldSize), random(-worldSize, worldSize), startingSize);
+    for (let i = 0; i < blobCount; i++) {
+        blobs[i] = new Blob(random(-worldSize, worldSize), random(-worldSize, worldSize), blobSize);
+    }
+    strokeCap(SQUARE);
+    stroke(0);
+}
+
+
+
+function draw() {
+    if (!logedin) return;
+    if (!prepareFirebase && logedin) {
+        database.ref('Users/' + uid).update({
+            display: username,
+            uid: uid,
+            x: Math.round(player.pos.x),
+            y: Math.round(player.pos.y),
+            size: Math.round(player.r),
+            killed: 0
+        });
+        let commentsRef = firebase.database().ref('Users');
+        commentsRef.on('value', function(data) {
+            players = data.val();
+        });
+        prepareFirebase = true;
+    }
+
+    displayHud();
+
+    // Prepare view
+    translate(width/2, height/2); // Center view
+    let newzoom = zoomScale / ((player.r - startingSize) / 2 + startingSize);
+    newzoom /= zoomCounter;
+    if (glitchCounter > 0) {
+        newzoom *= (Math.random() * 0.5) + 0.25;
+        // rotate(Math.random() * 10);
+        // zoom = newzoom;
+        zoom = lerp(zoom, newzoom, 0.1);
+    } else {
+        zoom = lerp(zoom, newzoom, 0.1);
+    }
+    scale(zoom);
+    translate(-player.pos.x, -player.pos.y);
+
+    // Draw world boarder lines
+    if (hitBorder) {
+        stroke(0, 100, 100);
+        player.r -= 0.5;
+        hitBorder = false;
+    } else {
+        stroke(0);
+    }
+    strokeWeight(10);
+    line(-worldBorder, -worldBorder, -worldBorder, worldBorder);
+    line(-worldBorder, worldBorder, worldBorder, worldBorder);
+    line(worldBorder, worldBorder, worldBorder, -worldBorder);
+    line(worldBorder, -worldBorder, -worldBorder, -worldBorder);
+    strokeWeight(0);
+
+    let viewDistance = zoom * width * 10;
+
+    player.renderPlayer();
+    if (prepareFirebase) {
+        player.update(player.r);
+    }
+
+    blobs.sort(compare);
+
+    fill(0);
+    if (magnetCounter > 0) {
+        for (let i = blobs.length - 1; i >= 0; i--) {
+            if (player.pos.x - blobs[i].pos.x > player.r + magnetRange) continue;
+            if (player.pos.y - blobs[i].pos.y > player.r + magnetRange) continue;
+            let dist = p5.Vector.dist(player.pos, blobs[i].pos);
+            if (Math.abs(dist) < player.r + magnetRange) {
+                console.log("yee");
+                let d = createVector(player.pos.x - blobs[i].pos.x, player.pos.y - blobs[i].pos.y);
+                d.setMag(magnetStrength);
+                blobs[i].pos.add(d);
+            }
+        }
+
+    } else {
+        // for (let i = blobs.length - 1; i >= 0; i--) {
+        //     if (player.pos.x - blobs[i].pos.x > player.r) continue;
+        //     if (player.pos.y - blobs[i].pos.y > player.r) continue;
+        //     let dist = p5.Vector.dist(player.pos, blobs[i].pos);
+        //     if (Math.abs(dist) < player.r) {
+        //         console.log("yee");
+        //         let d = createVector(player.pos.x - blobs[i].pos.x, player.pos.y - blobs[i].pos.y);
+        //         d.setMag(magnetStrength);
+        //         blobs[i].pos.add(d);
+        //     }
+        // }
+    }
+
+    for (let i = blobs.length - 1; i >= 0; i--) {
+        if (player.eats(blobs[i])) {
+            blobs.splice(i, 1);
+            blobs[blobs.length] = new Blob(random(-worldSize, worldSize), random(-worldSize, worldSize), blobSize)
+        } else {
+            currentPower = NONE;
+            blobs[i].render();
+        }
+    }
+
+    if (prepareFirebase) {
+        if (prevX !== player.pos.x || prevY !== player.pos.y) {
+            if (prevR === player.r) {
+                database.ref('Users/' + uid).update({
+                    x: Math.round(player.pos.x),
+                    y: Math.round(player.pos.y)
+                });
+            } else {
+                database.ref('Users/' + uid).update({
+                    x: Math.round(player.pos.x),
+                    y: Math.round(player.pos.y),
+                    size: Math.round(player.r)
+                });
+            }
+        }
+        if (players) {
+            for (let key in players) {
+                // Get values of other player
+                let other = players[key];
+                let otherUID = other["uid"];
+                let otherR = other["size"];
+                let otherDisplay = other["display"];
+                let amKilled = other["killed"];
+                let otherX = other["x"];
+                let otherY = other["y"];
+                if (otherUID === undefined) continue;
+                // Check if self
+                if (otherUID === uid) {
+                    // Check if dead
+                    if (amKilled === 1) {
+                        respawn();
+                    }
+                    continue;
+                }
+                // Display other player ellipse
+                if (!(otherUID in colorMap)) {
+                    colorMap[otherUID] = random(100);
+                }
+                if (glitchCounter > 0) {
+                    fill(Math.random() * 100, 100);
+                } fill(colorMap[otherUID], 100, 100);
+                ellipse(otherX, otherY, otherR * 2);
+
+                // Update high scores
+                highScores[otherUID] = Math.floor(otherR);
+
+                // Display hover text
+                textSize(otherR / 2);
+                fill(0);
+                textAlign(CENTER, CENTER);
+                text(otherDisplay.substr(0, otherDisplay.indexOf(" ")), otherX, otherY);
+
+                // Check collision
+                if (player.r > otherR) {
+                // if (false) {
+                    let d = p5.Vector.dist(player.pos, createVector(otherX, otherY));
+                    if (Math.abs(d) < player.r + otherR && !(otherUID in recentlyEaten)) {
+                        recentlyEaten[otherUID] = 30;
+                        database.ref('Users/' + otherUID).update({
+                            killed: 1
+                        });
+                        let sum = (sqrt(((PI * player.r * player.r) + (PI * otherR * otherR)) / PI));
+                        console.log(sum);
+                        player.r = sum;
+                    }
+                }
+            }
+        }
+    }
+    for (let key in recentlyEaten) {
+        if (recentlyEaten[key] < 0) {
+            delete recentlyEaten[key];
+            continue;
+        }
+        recentlyEaten[key] -= 1;
+    }
+
+    // User stats update
+    if (frameCount % 60 === 0) {
+        let totalPoints = 0;
+        firebase.database().ref('Stats/' + uid).once('value').then(function(snapshot) {
+            if (snapshot.val().totalPoints) {
+                totalPoints = snapshot.val().totalPoints;
+            }
+            let toAdd = player.r - lastScore;
+            if (lastScore < 0) toAdd = 0;
+            lifetimePoints = totalPoints += Math.ceil(toAdd);
+            database.ref('Stats/' + uid).update({
+                totalPoints: lifetimePoints
+            });
+            lastScore = player.r;
+        });
+    }
+
+    // Var updates
+    if (zoomCounter > 1) zoomCounter -= 0.1;
+    speedCounter -= 0.1;
+    magnetCounter -= 0.1;
+    if (glitchCounter > 0) rotate(0);
+    glitchCounter -= 0.1;
+    prevX = player.pos.x;
+    prevY = player.pos.y;
+    prevR = player.r;
+}
+
+
+/* Render HUD */
+function displayHud() {
+    if (glitchCounter > 0) {
+        background(Math.random() * 100, 100, 100);
+        fill(0);
+        textSize(32);
+        textAlign(LEFT, BASELINE);
+        text(("Score: " + Math.floor(player.r)).split('').sort(function(){return 0.5-Math.random()}).join(''), 10, 30);
+        textAlign(RIGHT);
+        text("v2.1".split('').sort(function(){return 0.5-Math.random()}).join(''), width, 30);
+
+        //Display leader board
+        highScores[uid] = Math.floor(player.r);
+        let bestPlayer = uid;
+        for (let key in highScores) {
+            if (highScores[key] > highScores[bestPlayer]) {
+                bestPlayer = key;
+            }
+        }
+        textAlign(LEFT);
+        // text(highScores[bestPlayer], 10, height - 5);
+        text(("Lifetime mass: " + lifetimePoints).split('').sort(function(){return 0.5-Math.random()}).join(''), 10, height - 5);
+
+        // Display FPS
+        countFPS += frameRate();
+        if (frameCount % 60 === 0) {
+            averageFPS = countFPS / 60;
+            countFPS = 0;
+        }
+        textAlign(RIGHT);
+        text(("FPS: " + Math.round(averageFPS)).split('').sort(function(){return 0.5-Math.random()}).join(''), width - 5, height - 5);
+    } else {
+        background(100);
+        fill(0);
+        textSize(32);
+        textAlign(LEFT, BASELINE);
+        text("Score: " + Math.floor(player.r), 10, 30);
+        textAlign(RIGHT);
+        text("v2.1", width, 30);
+
+        //Display leader board
+        highScores[uid] = Math.floor(player.r);
+        let bestPlayer = uid;
+        for (let key in highScores) {
+            if (highScores[key] > highScores[bestPlayer]) {
+                bestPlayer = key;
+            }
+        }
+        textAlign(LEFT);
+        // text(highScores[bestPlayer], 10, height - 5);
+        text("Alltime mass: " + lifetimePoints, 10, height - 5);
+
+        // Display FPS
+        countFPS += frameRate();
+        if (frameCount % 60 === 0) {
+            averageFPS = countFPS / 60;
+            countFPS = 0;
+        }
+        textAlign(RIGHT);
+        text("FPS: " + Math.round(averageFPS), width - 5, height - 5);
+    }
+}
+
+function compare(a,b) {
+    if (a.powerUp > b.powerUp)
+        return -1;
+    if (a.powerUp < b.powerUp)
+        return 1;
+    return 0;
+}
+
+function respawn() {
+    player.r = startingSize;
+    player.pos = createVector(random(-worldSize, worldSize), random(-worldSize, worldSize));
+    database.ref('Users/' + uid).update({
+        killed: 0
+    });
+}
+
+window.onresize = function () {
+    resizeCanvas(window.innerWidth,  window.innerHeight - document.getElementById('topbar').offsetHeight);
+};
---
+++ blob.js
@@ -0,0 +1,123 @@
+const NONE = 0;
+const SPEED = 1;
+const ZOOM = 2;
+const MAGNET = 3;
+const GLITCH = 4;
+const BIG = 5;
+
+let currentPower = NONE;
+
+function Blob(x, y, r) {
+    this.pos = createVector(x, y);
+    this.r = r;
+    this.vel = createVector(0, 0);
+    this.powerUp = getPowerUp();
+
+    this.update = function() {
+        let newvel = createVector(mouseX - width/2, mouseY - height/2);
+        let speedMulti = 1;
+        if (speedCounter > 0) speedMulti = 2;
+        if (newvel.mag() < zoomScale) {
+            newvel.setMag(1);
+        } else {
+            newvel.setMag(Math.max(playerSpeed * 2 - player.r * 0.03, playerSpeed) * speedMulti);
+        }
+        this.vel.lerp(newvel, 0.2);
+        let addedPos = p5.Vector.add(this.pos, newvel);
+        if (addedPos.y < -worldBorder + this.r) {
+            this.vel.y = 0;
+            hitBorder = true;
+            this.pos.y = -worldBorder + this.r;
+        }
+        if (addedPos.y > worldBorder - this.r) {
+            this.vel.y = 0;
+            hitBorder = true;
+            this.pos.y = worldBorder - this.r;
+        }
+        if (addedPos.x < -worldBorder + this.r) {
+            this.vel.x = 0;
+            hitBorder = true;
+            this.pos.x = -worldBorder + this.r;
+        }
+        if (addedPos.x > worldBorder - this.r) {
+            this.vel.x = 0;
+            hitBorder = true;
+            this.pos.x = worldBorder - this.r;
+        }
+        this.pos.add(this.vel);
+    };
+
+    this.eats = function (other) {
+        let radiusTotal = this.r + other.r;
+        if (this.pos.x - other.pos.x > radiusTotal) return false;
+        if (this.pos.y - other.pos.y > radiusTotal) return false;
+        let d = p5.Vector.dist(this.pos, other.pos);
+        if (Math.abs(d) < radiusTotal) {
+            // this.r += other.r;
+            if (other.powerUp === SPEED) speedCounter = 12;
+            if (other.powerUp === ZOOM) zoomCounter = 8;
+            if (other.powerUp === MAGNET) magnetCounter = 50;
+            if (other.powerUp === GLITCH) glitchCounter = 8;
+            if (other.powerUp === BIG) {
+                let sum = (PI * this.r * this.r) + (PI * other.r * other.r) * 20;
+                this.r = sqrt(sum / PI);
+            } else {
+                let sum = (PI * this.r * this.r) + (PI * other.r * other.r);
+                this.r = sqrt(sum / PI);
+            }
+            return true;
+        }
+        return false;
+    };
+
+    this.renderPlayer = function () {
+        if (player.r < 0) respawn(); // Respawn if mass less than 0
+
+        // Display player
+        if (glitchCounter > 0) {
+            fill(Math.random() * 100, 100);
+        } else fill(mycolor, 100, 100);
+        ellipse(this.pos.x, this.pos.y, this.r * 2, this.r * 2);
+
+        // Display hover text
+        textSize(this.r / 2);
+        fill(0);
+        textAlign(CENTER, CENTER);
+        if (glitchCounter > 0 ) {
+            text((username.substr(0, username.indexOf(" "))).split('').sort(function(){return 0.5-Math.random()}).join(''), this.pos.x, this.pos.y);
+        } else text(username.substr(0, username.indexOf(" ")), this.pos.x, this.pos.y);
+    };
+
+    this.render = function () {
+        if (glitchCounter > 0) fill(Math.random() * 100, 100, 100);
+        if (this.powerUp !== currentPower) {
+            getColor(this.powerUp);
+            currentPower = this.powerUp;
+        }
+        ellipse(this.pos.x, this.pos.y, this.r * 2, this.r * 2);
+    };
+}
+
+function getColor(x) {
+    if (x === ZOOM) {
+        fill(33, 100, 100);
+    } else if (x === MAGNET) {
+        fill(72, 100, 100);
+    } else if (x === GLITCH) {
+        fill(48, 100, 100);
+    } else if (x === BIG) {
+        fill(0, 100, 100);
+    } else {
+        fill(66, 100, 100);
+    }
+}
+
+function getPowerUp() { // OPT: Clear
+    let rnd = Math.random();
+    if (rnd > 0.015) return NONE;
+    if (rnd > 0.01) return BIG;
+    if (rnd > 0.005) return SPEED;
+    if (rnd > 0.003) return MAGNET;
+    if (rnd > 0.0001) return ZOOM;
+    return GLITCH;
+}
